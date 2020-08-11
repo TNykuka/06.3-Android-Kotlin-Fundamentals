e4e52b7ad8fa4437b79cbf74b7a31c350a32df98
TNyKk <kingkuka088@gmail.com>
2020-08-11 14:17:48 +0700

    Android Kotlin Fundamentals 06.3-5 Step 1: Update button states

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
@@ -127,5 +127,14 @@ class SleepTrackerViewModel(
         _navigateToSleepQuality.value = null
     }
 
+    val startButtonVisible = Transformations.map(tonight) {
+        it == null
+    }
+    val stopButtonVisible = Transformations.map(tonight) {
+        it != null
+    }
+    val clearButtonVisible = Transformations.map(nights) {
+        it?.isNotEmpty()
+    }
 }
 
